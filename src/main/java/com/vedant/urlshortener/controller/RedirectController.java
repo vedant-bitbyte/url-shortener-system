79860fb07839754999691da89af07beb27bf984b
Vedant Pawar <vcpawar1603@gmail.com>
2026-05-25 23:30:26 +0530

    Added URL redirect system

--- src/main/java/com/vedant/urlshortener/controller/RedirectController.java
+++ src/main/java/com/vedant/urlshortener/controller/RedirectController.java
@@ -1,4 +1,23 @@
 package com.vedant.urlshortener.controller;
 
+import com.vedant.urlshortener.service.UrlService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
 public class RedirectController {
+
+    private final UrlService urlService;
+
+    public RedirectController(UrlService urlService) {
+        this.urlService = urlService;
+    }
+
+    @GetMapping("/{shortCode}")
+    public RedirectView redirect(@PathVariable String shortCode) {
+        String originalUrl = urlService.redirectToOriginalUrl(shortCode);
+        return new RedirectView(originalUrl);
+    }
 }
--- src/main/java/com/vedant/urlshortener/controller/UrlController.java
+++ src/main/java/com/vedant/urlshortener/controller/UrlController.java
@@ -4,14 +4,13 @@ import com.vedant.urlshortener.dto.ShortenUrlRequest;
 import com.vedant.urlshortener.service.UrlService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
+@RequestMapping("/api/url")
 public class UrlController {
 
     private final UrlService urlService;
@@ -20,15 +19,9 @@ public class UrlController {
         this.urlService = urlService;
     }
 
-    @PostMapping("/api/url/shorten")
+    @PostMapping("/shorten")
     public ResponseEntity<String> shorten(@Valid @RequestBody ShortenUrlRequest request) {
         String shortenedUrl = urlService.createShortUrl(request);
         return ResponseEntity.ok(shortenedUrl);
-    }
-
-    @GetMapping("/{shortCode}")
-    public RedirectView redirect(@PathVariable String shortCode) {
-        String originalUrl = urlService.redirectToOriginalUrl(shortCode);
-        return new RedirectView(originalUrl);
     }
 }
