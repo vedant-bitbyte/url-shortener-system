ff4ea0273b8fcb44c8134eac4560895760bbd8d2
Vedant Pawar <vcpawar1603@gmail.com>
2026-05-25 22:39:48 +0530

    Added secret key to enviornment variable

--- src/main/java/com/vedant/urlshortener/security/JwtService.java
+++ src/main/java/com/vedant/urlshortener/security/JwtService.java
@@ -2,6 +2,7 @@ package com.vedant.urlshortener.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -12,8 +13,8 @@ import java.util.Date;
 public class JwtService {
 
     // TODO: move to application.properties / environment variable
-    private static final String SECRET_KEY =
-            "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     private static final long EXPIRATION_MS = 24 * 60 * 60 * 1000L;
 
